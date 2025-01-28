8e6db3d710b0f602550228ebbb3d18ea6b207c7f
Pascual Felicio <pascualfelicio@gmail.com>
2025-01-28 13:39:04 +0100

    Continue

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const userImg = document.querySelectorAll(".img-choice");
 const pcChoiceImg = document.querySelector(".display-pc");
 const userChoiceImg = document.querySelector(".display-user");
-
-const pcChoice = () => {
-    const pcArray = ['paper', 'rock', 'scissors'];
-    return pcArray[Math.floor(Math.random() * pcArray.length)]
-}
-
-userImg.forEach((choice) => {
-    choice.addEventListener(('click'), (e) => {
-        const userChoice = e.target.attributes.value.value
-        const pcPlay = pcChoice()
-        pcChoiceImg.src = `images/${pcPlay}.png`;
-        userChoiceImg.src = `images/${userChoice}.png`;
-        console.log(checkWinner(userChoice, pcPlay))
-    })
-})
+const btnStartGame = document.querySelector(".btn-start-game");
+const containerStartGame = document.querySelector(".container");
+const pcScore = document.querySelector(".pcScore");
+const userScore = document.querySelector(".userScore");
+const displaySentence = document.querySelector(".sentence")
 
 const checkWinner = (userChoice, pcChoice) => {
     let winner = '';
@@ -27,7 +17,7 @@ const checkWinner = (userChoice, pcChoice) => {
         return winner = 'userWin'
     } else if (userChoice === 'rock' && pcChoice === 'paper' ||
         userChoice === 'paper' && pcChoice === 'scissors' ||
-        userChoice === 'scissors' && pcChoice === 'rock')   // winning user
+        userChoice === 'scissors' && pcChoice === 'rock')   // winning pc
     {
         return winner = 'pcWin'
     } else { //tie
@@ -35,15 +25,53 @@ const checkWinner = (userChoice, pcChoice) => {
     }
 }
 
+btnStartGame.addEventListener(('click'), (e) => {
+    containerStartGame.style.display = 'flex';
+    btnStartGame.style.display = 'none';
+    game();
+});
 
 
 const game = () => {
-    let gameOn = true;
-    let scoreUser = 0;
-    let scorePc = 0;
+    let gameOn = true
+    let userScoring = 0;
+    let pcScoring = 0;
 
-    while (gameOn) {
+    while(gameOn) {
 
-        gameOn = false;
+        const pcChoice = () => {
+            const pcArray = ['paper', 'rock', 'scissors'];
+            return pcArray[Math.floor(Math.random() * pcArray.length)]
+        }
+        
+        userImg.forEach((choice) => {
+            choice.addEventListener(('click'), (e) => {
+        
+                const userChoice = e.target.attributes.value.value;
+                const pcPlay = pcChoice();
+                pcChoiceImg.src = `images/${pcPlay}.png`;
+                userChoiceImg.src = `images/${userChoice}.png`;
+                
+                const winnerRound = checkWinner(userChoice, pcPlay)
+        
+                displaySentence.textContent = winnerRound;
+        
+                if (winnerRound === 'tie') {
+                    displaySentence.textContent = 'It is tie';
+                } else if (winnerRound === 'pcWin') {
+                    pcScoring++
+                    displaySentence.textContent = 'PC won!';
+                    pcScore.textContent = `PC: ${pcScoring}`
+                } else {
+                    userScoring++
+                    displaySentence.textContent = 'You won!';
+                    userScore.textContent = `User: ${userScoring}`
+                }
+        
+                console.log(checkWinner(userChoice, pcPlay))
+            })
+        })
+
+        gameOn = false
     }
 }
